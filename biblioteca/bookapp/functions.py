f637c8b207851b8f9cfc5483a9dbedb8269c7d78
Andres Melendez <ia.andresalexander@ufg.edu.sv>
2022-07-21 23:33:17 -0600

    Se eliminó función no utilizada

--- biblioteca/bookapp/functions.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-def handle_uploaded_file(f):
-    route = Path('bookapp/static/upload')
-    with open(route.joinpath(f.name), 'wb+') as destination:  
-        for chunk in f.chunks():  
-            destination.write(chunk)  
